d2ae30ea14547e973320f926d5695833cbaa9c22
GathangaElijah <elyg3672@gmail.com>
2024-12-14 02:13:23 +0300

    fectch the map data

--- mapping/fetch-maps.go
+++ mapping/fetch-maps.go
@@ -10,36 +10,53 @@ import (
 // OSM stands for OpenStreet Maps- This is geolocation maps
 // that am using get the coordinates of a subregion
 type OSM struct {
-	XMLName   xml.Name   `xml:"osm"`
 	Version   string     `xml:"version,attr"`
 	Generator string     `xml:"generator,attr"`
+	Nodes     []Node     `xml:"node"`
+	Ways      []Way      `xml:"way"`
 	Relations []Relation `xml:"relation"`
 }
 
-type Relation struct {
-	ID        int64    `xml:"id,attr"`
-	Visible   bool     `xml:"visible,attr"`
-	Version   int      `xml:"version,attr"`
-	Changeset int64    `xml:"changeset,attr"`
-	Timestamp string   `xml:"timestamp,attr"`
-	User      string   `xml:"user,attr"`
-	UID       int64    `xml:"uid,attr"`
-	Members   []Member `xml:"member"`
-	Tags      []Tag    `xml:"tag"`
+// Node represents an OSM node with latitude and longitude.
+type Node struct {
+	ID  int64   `xml:"id,attr"`
+	Lat float64 `xml:"lat,attr"`
+	Lon float64 `xml:"lon,attr"`
 }
 
+// Way represents an OSM way with references to nodes.
+type Way struct {
+	ID    int64     `xml:"id,attr"`
+	Nodes []WayNode `xml:"nd"`
+	Tags  []Tag     `xml:"tag"`
+}
+
+// WayNode represents a node reference in a way.
+type WayNode struct {
+	Ref int64 `xml:"ref,attr"`
+}
+
+// Relation represents an OSM relation.
+type Relation struct {
+	ID      int64    `xml:"id,attr"`
+	Members []Member `xml:"member"`
+	Tags    []Tag    `xml:"tag"`
+}
+
+// Member represents a member of a relation.
 type Member struct {
 	Type string `xml:"type,attr"`
 	Ref  int64  `xml:"ref,attr"`
 	Role string `xml:"role,attr"`
 }
 
+// Tag represents a key-value pair in OSM data.
 type Tag struct {
 	Key   string `xml:"k,attr"`
 	Value string `xml:"v,attr"`
 }
 
-// This functions returns the raw data from the xml file which 
+// This functions returns the raw data from the xml file which
 // has data of a particular region.
 func ParseOSMFile(filename string) (*OSM, error) {
 	// opening the osm file
@@ -49,11 +66,12 @@ func ParseOSMFile(filename string) (*OSM, error) {
 	}
 	defer file.Close()
 
-	var osm OSM
+	var osmData OSM
 	decoder := xml.NewDecoder(file)
-	err = decoder.Decode(&osm)
+
+	err = decoder.Decode(&osmData)
 	if err != nil {
 		return nil, err
 	}
-	return &osm, nil
+	return &osmData, nil
 }
