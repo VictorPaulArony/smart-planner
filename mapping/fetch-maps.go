4fefee10fb7115978528429caa06408532f94d95
GathangaElijah <elyg3672@gmail.com>
2024-12-14 19:25:51 +0300

    feat: function to handle api data

--- mapping/fetch-maps.go
+++ mapping/fetch-maps.go
@@ -1,77 +1,32 @@
 package mapping
 
 import (
-	"encoding/xml"
-	"os"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
 )
 
-// This structure parses the Overpass Data json response
-
-// OSM stands for OpenStreet Maps- This is geolocation maps
-// that am using get the coordinates of a subregion
-type OSM struct {
-	Version   string     `xml:"version,attr"`
-	Generator string     `xml:"generator,attr"`
-	Nodes     []Node     `xml:"node"`
-	Ways      []Way      `xml:"way"`
-	Relations []Relation `xml:"relation"`
-}
-
-// Node represents an OSM node with latitude and longitude.
-type Node struct {
-	ID  int64   `xml:"id,attr"`
-	Lat float64 `xml:"lat,attr"`
-	Lon float64 `xml:"lon,attr"`
-}
-
-// Way represents an OSM way with references to nodes.
-type Way struct {
-	ID    int64     `xml:"id,attr"`
-	Nodes []WayNode `xml:"nd"`
-	Tags  []Tag     `xml:"tag"`
-}
-
-// WayNode represents a node reference in a way.
-type WayNode struct {
-	Ref int64 `xml:"ref,attr"`
-}
-
-// Relation represents an OSM relation.
-type Relation struct {
-	ID      int64    `xml:"id,attr"`
-	Members []Member `xml:"member"`
-	Tags    []Tag    `xml:"tag"`
-}
-
-// Member represents a member of a relation.
-type Member struct {
-	Type string `xml:"type,attr"`
-	Ref  int64  `xml:"ref,attr"`
-	Role string `xml:"role,attr"`
-}
-
-// Tag represents a key-value pair in OSM data.
-type Tag struct {
-	Key   string `xml:"k,attr"`
-	Value string `xml:"v,attr"`
-}
-
-// This functions returns the raw data from the xml file which
-// has data of a particular region.
-func ParseOSMFile(filename string) (*OSM, error) {
-	// opening the osm file
-	file, err := os.Open(filename)
+func KisumuMap(url, query string) (interface{}, error) {
+	// Send request
+	resp, err := http.Post(url, "text/plain", bytes.NewBufferString(query))
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Error making request to Overpass API")
 	}
-	defer file.Close()
+	defer resp.Body.Close()
 
-	var osmData OSM
-	decoder := xml.NewDecoder(file)
-
-	err = decoder.Decode(&osmData)
+	// Read response
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Error reading response")
 	}
-	return &osmData, nil
+
+	// Parse JSON response
+	var data interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, errors.New("Error parsing JSON response")
+	}
+	return data, nil
 }
