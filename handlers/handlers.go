696939b8987acecd05794f83de53342d424181f2
viarony <smallypauls@gmail.com>
2024-12-14 16:33:51 +0300

    handlers/handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -9,6 +9,9 @@ import (
 // Template loader
 var templates = template.Must(template.New("").ParseFiles(
 	"templates/index.html",
+	"templates/case.html",
+	"templates/contact.html",
+	"templates/features.html",
 ))
 
 // Handler serves requests for Vercel
@@ -30,4 +33,17 @@ func renderTemplate(w http.ResponseWriter, templateName string, data interface{}
 		log.Println("Template error:", err)
 		http.Error(w, "Template rendering error", http.StatusInternalServerError)
 	}
+}
+
+// function to handle other pages of the website
+func FeaturesHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "features.html", nil)
+}
+
+func CaseHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "case.html", nil)
+}
+
+func ContactHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact.html", nil)
 }
