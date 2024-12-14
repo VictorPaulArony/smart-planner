2a3bb2b0cb959f4a9ab8b0f55e71099a7b22095f
viarony <smallypauls@gmail.com>
2024-12-14 16:27:09 +0300

    direct changes in the main file to avoid conflict in the main.go file

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	mapping "smart-planner/mapping"
 
 	"smart-planner/handlers"
 )
@@ -14,33 +11,9 @@ import (
 // function main of the smart planners system
 func main() {
 	// The output of the map data.
-	osmFile := "kisumu-data.xml"
-	osm, err := mapping.ParseOSMFile(osmFile)
-	if err != nil {
-		fmt.Println("Error parsing OSM file:", err)
-		return
-	}
-	// "This is the raw data"
-	fmt.Println("This is my osm data\n", "<<<", osm)
-
-	// Print the ralation details
-	for _, relation := range osm.Relations {
-		fmt.Printf("Relation ID: %d, Visible: %v, Version: %d, User: %s\n",
-			relation.ID, relation.Visible, relation.Version, relation.User)
-		fmt.Println("Members:")
-		for _, member := range relation.Members {
-			fmt.Printf(" - Type: %s, Ref: %d, Role: %s\n", member.Type, member.Ref, member.Role)
-		}
-		for _, tag := range relation.Tags {
-			fmt.Printf(" - %s = %s\n", tag.Key, tag.Value)
-		}
-	}
+	// osmFile := "kisumu-data.xml"
 
 	// functionality to handle the static files
-	// fs := http.FileServer(http.Dir("static"))
-	// http.Handle("/static/", http.StripPrefix("/static", fs))
-
-	// main page handler functions
 	http.HandleFunc("/", handlers.IndexHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
