7412105a0108f84d556c8645014cc7d0a8a11e8a
viarony <smallypauls@gmail.com>
2024-12-14 16:16:57 +0300

    main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"smart-planner/handlers"
 )
@@ -11,15 +12,17 @@ import (
 func main() {
 	// functionality to handle the static files
 	// fs := http.FileServer(http.Dir("static"))
-	// http.Handle("/static/", http.StripPrefix("/static", fs))
-
+	// http.Handle("/static/", http.StripPrefix("/static", fs
 	// main page handler functions
 	http.HandleFunc("/", handlers.IndexHandler)
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "3000" // Default fallback for local testing
-	// }
+	http.HandleFunc("/features", handlers.FeaturesHandler)
+	http.HandleFunc("/contact", handlers.ContactHandler)
+	http.HandleFunc("/case", handlers.CaseHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // Default fallback for local testing
+	}
 	// localhost for the system testing
 	log.Println("http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+port, nil)
 }
